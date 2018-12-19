317839332d06f81d41c7a9ef1cd3e40b41edddf9
yangj <846272692@qq.com>
2018-12-19 23:56:10 +0800

    the framework

---
+++ src/main/java/com/soft1841/sm/Main.java
@@ -0,0 +1,26 @@
+package com.soft1841.sm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        Scene scene = new Scene(root,1000,600);
+        scene.getStylesheets().addAll("/css/login.css");
+        primaryStage.setTitle("登陆界面");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
