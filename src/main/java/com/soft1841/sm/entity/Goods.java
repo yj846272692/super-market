91073c399d9e63ffcc044fe73160a0d428068568
javafx study <zhaojiayue2000@gmail.com>
2018-12-23 18:15:28 +0800

    2018/12/23

--- src/main/java/com/soft1841/sm/entity/Goods.java
+++ src/main/java/com/soft1841/sm/entity/Goods.java
@@ -1,4 +1,14 @@
 package com.soft1841.sm.entity;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Goods {
+    private final SimpleLongProperty id = new SimpleLongProperty();
+    private final SimpleLongProperty typeId = new SimpleLongProperty();
+    private final SimpleStringProperty name = new SimpleStringProperty("");
+    private final SimpleDoubleProperty price =new SimpleDoubleProperty();
+    private final SimpleStringProperty piture = new SimpleStringProperty("");
+    private final SimpleStringProperty description = new SimpleStringProperty("");
 }
